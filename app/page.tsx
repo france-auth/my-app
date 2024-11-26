ce892fb8e5b59461ba1ebcd8d75b374b350b6c88
Levi <70198955+Draysongz@users.noreply.github.com>
2024-11-26 10:28:17 +0100

    ddd

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ function HomeContent() {
   //   "query_id=AAElBO5_AAAAACUE7n8MOa_y&user=%7B%22id%22%3A2146305061%2C%22first_name%22%3A%22Crypto%22%2C%22last_name%22%3A%22Dray%22%2C%22username%22%3A%22Habibilord%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1729828440&hash=a21590e0fe10c68048781c3b3e22e8ecde0a8b8b163bf4c7a58e5c48855e584e";
 
   useEffect(() => {
-    WebApp.expand
+    WebApp.expand()
     const telegramInitData = WebApp.initData
     if (telegramInitData) {
       fetch("/api/auth", {
